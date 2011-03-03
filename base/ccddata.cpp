776f91999ce4a51b0d2cb23dcef193f9baeacb61
Daniel Wagenaar <daw@caltech.edu>
2011-03-03 15:26:32 -0800

    Refined the checkout/checkin mechanism in CCDData to allow multiple keys.

--- base/ccddata.cpp
+++ base/ccddata.cpp
@@ -5,6 +5,9 @@
 #include <base/memalloc.h>
 #include <base/dbg.h>
 
+class CCDData::CheckoutKey {
+};
+
 CCDData::CCDData(int serpix_, int parpix_, int nframes_) {
   serpix = serpix_;
   parpix = parpix_;
@@ -15,7 +18,7 @@ CCDData::CCDData(int serpix_, int parpix_, int nframes_) {
   data = memalloc<uint16_t>(allocpix, "CCDData:: constructor");
   t0_ms = 0;
   dt_ms = 0; // default value: meaningless on purpose
-  checkedout = false;
+  mustEmit = false;
 }
 
 bool CCDData::reshape(int ser, int par, int nfr, bool free) {
@@ -31,8 +34,7 @@ bool CCDData::reshape(int ser, int par, int nfr, bool free) {
   parpix = par;
   framepix = ser*par;
   nframes = nfr;
-  if (!checkedout)
-    emit newData();
+  emitUnlessCheckedOut();
   return realloc;
 }
 
@@ -49,7 +51,7 @@ uint16_t const *CCDData::frameData(int frame) const {
   return data + frame*framepix;
 }
 
-uint16_t *CCDData::frameData(CheckoutKey, int frame) {
+uint16_t *CCDData::frameData(CheckoutKey *, int frame) {
   if (frame<0)
     return data;
   if (frame>=nframes)
@@ -57,31 +59,47 @@ uint16_t *CCDData::frameData(CheckoutKey, int frame) {
   return data + frame*framepix;
 }
 
+void CCDData::emitUnlessCheckedOut() {
+  if (keys.isEmpty()) {
+    mustEmit = false;
+    emit newData();
+  } else {
+    mustEmit = true;
+  }
+}  
+
 void CCDData::setTimeBase(double t0, double dt) {
   t0_ms = t0;
   dt_ms = dt;
-  if (!checkedout)
-    emit newData();
+  emitUnlessCheckedOut();
 }
 
 void CCDData::setDataToCanvas(Transform const &t0) {
   t = t0;
-  if (!checkedout)
-    emit newData();
+  emitUnlessCheckedOut();
 }
 
-CCDData::CheckoutKey CCDData::checkout() {
-  checkedout = true;
-  return CheckoutKey();
+CCDData::CheckoutKey *CCDData::checkout() {
+  CheckoutKey *key = new CheckoutKey();
+  keys.insert(key);
+  return key;
 }
 
-void CCDData::cancel(CCDData::CheckoutKey) {
-  checkedout = false;
+void CCDData::cancel(CCDData::CheckoutKey *key) {
+  if (!keys.contains(key))
+    throw Exception("CCDData", "Canceling with unknown key");
+  keys.remove(key);
+  delete key;
+  if (mustEmit)
+    emitUnlessCheckedOut();
 }
 
-void CCDData::checkin(CCDData::CheckoutKey) {
-  checkedout = false;
-  emit newData();
+void CCDData::checkin(CCDData::CheckoutKey *key) {
+  if (!keys.contains(key))
+    throw Exception("CCDData", "Checking in with unknown key");
+  keys.remove(key);
+  delete key;
+  emitUnlessCheckedOut();
 }
 
 CCDDataWriter::CCDDataWriter(CCDData &data): data(data) {
@@ -100,6 +118,6 @@ CCDDataWriter::~CCDDataWriter() {
     data.checkin(key);
 }
 
-uint16_t CCDDataWriter::frameData(int frame) {
+uint16_t *CCDDataWriter::frameData(int frame) {
   return data.frameData(key, frame);
 }
--- base/ccddata.h
+++ base/ccddata.h
@@ -7,6 +7,7 @@
 #include <base/types.h>
 #include <base/transform.h>
 #include <QObject>
+#include <QSet>
 
 class CCDData: public QObject {
   Q_OBJECT;
@@ -14,25 +15,49 @@ public:
   CCDData(int serpix=512, int parpix=512, int nframes=1);
   virtual ~CCDData();
 public:
-  class CheckoutKey { };
-  CheckoutKey checkout();
-  void checkin(CheckoutKey);
-  void cancel(CheckoutKey);
-  uint16_t *frameData(CheckoutKey, int frame=-1);
+  class CheckoutKey;
+  CheckoutKey *checkout();
+  void checkin(CheckoutKey *);
+  void cancel(CheckoutKey *);
+  /*:F checkout, checkin, cancel
+   *:D To gain write access to the frame data, you must request a key using
+       checkout(). The contents of the key is opaque. Once you're done writing,
+       call checkin() with the same key. If you decide not to write, call
+       cancel().
+       newData() is emitted upon final checkin.
+   *:N Slightly counterintuitively, if two clients check out, and one checks
+       in first after which the other cancels, a signal is emitted at the
+       time the second client cancels.
+   *:N Do *not* delete keys yourself. Call either checkin or cancel.
+   *:N checkout() does not guarantee exclusive access.
+   */
+  uint16_t *frameData(CheckoutKey *, int frame=-1);
   bool reshape(int serpix, int parpix, int nframes, bool free=false);
   /*:F reshape
    *:D Reshapes the data buffer. This may claim more space if needed; it
        will not release any unneeded space unless FREE is set to true.
    *:A True if reallocation occurred.
+   *:N Emits newData() unless a key has been checked out.
    */
   void setTimeBase(double t0_ms, double dt_ms);
   /*:F setTimeBase
    *:D Store timing information with the data.
+   *:N Emits newData() unless a key has been checked out.
   */
   void setDataToCanvas(Transform const &t);
+  /*:F setDataToCanvas
+   *:D Specifies how the image should be placed on the global canvas.
+   *:N Emits newData() unless a key has been checked out.
+  */
 public:
-  Transform const &dataToCanvas() const { return t; }
   uint16_t const *frameData(int frame=-1) const;
+  /*:F frameData
+   *:D Returns a pointer to the n-th frame.
+       Throws an exception if a non-existing frame is accessed.
+       Without an argument, returns a pointer to the first frame, and no
+       exception is thrown even if there are zero frames.
+   */
+  Transform const &dataToCanvas() const { return t; }
   int getSerPix() const { return serpix; }
   int getParPix() const { return parpix; }
   int getNFrames() const { return nframes; }
@@ -43,6 +68,8 @@ public:
 signals:
   void newData();
 private:
+  void emitUnlessCheckedOut();
+private:
   int serpix;
   int parpix;
   int nframes;
@@ -51,10 +78,24 @@ private:
   uint16_t *data;
   double t0_ms, dt_ms;
   Transform t;
-  bool checkedout;
+  QSet<CheckoutKey *> keys;
+  bool mustEmit;
 };
 
 class CCDDataWriter {
+  /*:C CCDDataWriter
+   *:D Convenience class for writing the data in a CCDData object.
+       Instead of writing:
+         CCDData::CheckoutKey *key = ccddata.checkout();
+	 uint16_t *tenthframe = ccddata.frameData(key, 10);
+	 // write to tenthframe
+	 ccddata.checkin(key);
+       you can write
+         CCDDataWriter wrt(ccddata);
+	 uint16_t *tenthframe = wrt.frameData(10);
+	 // write to tenthframe
+       That way, checkin is automatic when wrt goes out of scope.
+  */
 public:
   CCDDataWriter(CCDData &data);
   ~CCDDataWriter();
@@ -62,7 +103,7 @@ public:
   void cancel();
 private:
   CCDData &data;
-  CCDData::CheckoutKey key;
+  CCDData::CheckoutKey *key;
   bool canceled;
 };
 
