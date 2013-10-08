568d08ab14b336cd1309e83c397a172265c1c1ab
Daniel Wagenaar <daw@caltech.edu>
2013-10-08 15:08:28 -0400

    Windows scripts changed slightly

---
+++ vscope.bat
@@ -0,0 +1,6 @@
+@echo off
+
+C:
+chdir C:\cygwin\bin
+
+bash --login -i c:\cygwin\home\User\progs\vscope\indycam\vscope.win.sh
---
+++ vscope.win.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd $HOME/progs/vscope/indycam
+./vscope.exe
