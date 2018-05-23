2cf2a133e5924440c9133e7bb47e54219e7043a9
Daniel A. Wagenaar <daw@caltech.edu>
2018-05-23 12:44:25 -0700

    Copied tools/pbuild.sh from debian branch

---
+++ tools/pbuild.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+# pbuild - build for other version of ubuntu
+
+DIST=xenial
+
+BASETGZ=/data/dw/pbuilder/$DIST.tgz
+RESULT=../releases
+DSC=`ls $RESULT/*dsc | tail`
+
+mkdir $RESULT/$DIST
+
+echo Working with $DSC
+echo
+echo pbuilder build --basetgz $BASETGZ --distribution $DIST --buildresult $RESULT/$DIST $DSC
+sudo pbuilder build --basetgz $BASETGZ --distribution $DIST --buildresult $RESULT/$DIST $DSC
